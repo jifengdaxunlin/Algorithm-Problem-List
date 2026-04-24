9b7936c22256f171524c377134ca27812c45c818
jifengdaxunlun <jifengdaxunlin@outlook.com>
2026-04-24 21:15:13 +0800

    空样本修改

--- 滑动窗口与双指针/定长滑动窗口/00-空样本.cpp
+++ 滑动窗口与双指针/定长滑动窗口/00-空样本.cpp
@@ -13,22 +13,22 @@ public:
     }
 };
 
-    int main(int argc, char const *argv[])
+int main(int argc, char const *argv[])
+{
+    srand((unsigned)time(NULL));
+    clock_t start = clock(); // 表示当前cpu记录下的已经过了的毫秒数
+
+    Solution so;
+    int array_len = 10, k = 3; // 数组长度、窗口长度
+    vector<int> num_array(array_len);
+    for (int i = 0; i < array_len; i++)
     {
-        srand((unsigned)time(NULL));
-        clock_t start = clock(); // 表示当前cpu记录下的已经过了的毫秒数
-
-        Solution so;
-        int array_len = 10, k = 3; // 字符串数组长度、窗口长度
-        vector<int> num_array(array_len);
-        for (int i = 0; i < array_len; i++)
-        {
-            num_array[i] = rand() % 20001;
-            cout << num_array[i] << "\t";
-        }
-        cout << endl;
-
-        clock_t end = clock();
-        printf("\n算法运行了 %d ms", end - start);
-        return 0;
+        num_array[i] = rand() % 20001;
+        cout << num_array[i] << "\t";
     }
+    cout << endl;
+
+    clock_t end = clock();
+    printf("\n算法运行了 %d ms", end - start);
+    return 0;
+}
