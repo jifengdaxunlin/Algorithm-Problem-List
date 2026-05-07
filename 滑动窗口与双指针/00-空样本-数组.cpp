a0c34aa505078bb8ab793f82885d550d6b9479c0
jifengdaxunlun <jifengdaxunlin@outlook.com>
2026-05-07 20:08:14 +0800

    样本添加

---
+++ 滑动窗口与双指针/00-空样本-数组.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <ctime> // time()
+#include <unordered_map>    //哈希表头文件
+#include <cmath>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int sample(){
+
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    srand((unsigned)time(NULL));
+    clock_t start = clock(); // 表示当前cpu记录下的已经过了的毫秒数
+
+    Solution so;
+    int array_len = 10, k = 3; // 数组长度、窗口长度
+    vector<int> num_array(array_len);
+    for (int i = 0; i < array_len; i++)
+    {
+        num_array[i] = rand() % 20001;
+        cout << num_array[i] << "\t";
+    }
+    cout << endl;
+
+    clock_t end = clock();
+    printf("\n算法运行了 %d ms", end - start);
+    return 0;
+}
